c284c1c8f480d69bf3bb57bc90a1de35bde40764
joyce0803 <joyce08merin@gmail.com>
2023-04-29 19:14:43 +0530

    modified with menu modifiedd

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -46,16 +46,6 @@ const Menu = () => {
       });
   }, [id]);
 
-  const types = {
-    0: "Veg Pizza",
-    1: "Non-Veg Pizza",
-  };
-
-  const vegPizzas = menuItems.filter((item) => item.dish_type === "Veg Pizza");
-  const nonVegPizzas = menuItems.filter(
-    (item) => item.dish_type === "Non-Veg Pizza"
-  );
-
   return (
     <div className="listMenu">
       <div className="listContainer">
@@ -100,77 +90,10 @@ const Menu = () => {
         </div>
       </div>
 
-      {/* <Grid
-        container
-        spacing={4}
-        className="cards"
-        justifyContent="space-between"
-        ml="10px"
-        mt="20px"
-      >
-        {menuItems.map((items, index) => (
-          <Grid items xs={12} sm={6} md={4} lg={3} key={items._id}>
-            <Card
-              variant="outlined"
-              className="menuData"
-              sx={{
-                maxWidth: 300,
-                height: 90,
-                margin: "10px",
-                backgroundColor: "#0F1E1E",
-                borderRadius: "10px",
-                WebkitBoxShadow: "3px 3px 5px 4px rgba(9,92,92,0.39)",
-                boxShadow: "3px 3px 5px 4px rgba(9,92,92,0.19)",
-                MozBoxShadow: "3px 3px 5px 4px rgba(9,92,92,0.39)",
-                ":hover": {
-                  backgroundColor: "#003C3C",
-                },
-              }}
-            >
-              <CardContent sx={{ overflow: "auto" }}>
-                <Typography sx={{ fontSize: "14px" }}>
-                  {items.dish_name}
-                </Typography>
-                <div
-                  style={{ display: "flex", justifyContent: "space-between" }}
-                >
-                  <div>
-                    <Typography
-                      variant="h6"
-                      sx={{ paddingTop: "15px", color: "#92FF76" }}
-                    >
-                      ₹ {items.dish_price}
-                    </Typography>
-                  </div>
-                  <div
-                    style={{
-                      justifyContent: "flex-end",
-                      display: "flex",
-                    }}
-                  >
-                    <Typography
-                      sx={{
-                        fontSize: "10px",
-                        paddingTop: "20px",
-                        color: "#F0FF76",
-                      }}
-                    >
-                      {items.dish_type}
-                    </Typography>
-                  </div>
-                </div>
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
-      </Grid> */}
+
 
       <div style={{ margin:"20px"}}>
-        {/* <div style={{ textAlign: "center" }}>
-          <Typography fontWeight="600" color="black" variant="h5">
-            {name} Menu
-          </Typography>
-        </div> */}
+       
         <Grid
           container
           mt="20px"
